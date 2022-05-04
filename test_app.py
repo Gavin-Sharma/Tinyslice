99cff938a1d5536ca0425f2156de73101731a7a3
Brian Xian <brianxian2607@gmail.com>
2022-05-04 16:01:13 -0700

    created test for api endpoint

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import pytest, flask, requests
+
+@pytest.fixture
+def client():
+    response = requests.get("http://localhost:5000/api/grocery_list")
+    data = response.json()
+    assert response.status_code == 200
+    assert data[0]["name"] == "Apple"
+    

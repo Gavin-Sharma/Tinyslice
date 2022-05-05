5dd2b256842a15fd61ad5488243cc93f8024ca8f
Brian <bxian@my.bcit.ca>
2022-05-05 13:32:39 -0700

    updated test_app

--- test_app.py
+++ test_app.py
@@ -1,7 +1,6 @@
 import pytest, flask, requests
 
-@pytest.fixture
-def client():
+def test_api_grocery_list():
     response = requests.get("http://localhost:5000/api/grocery_list")
     data = response.json()
     assert response.status_code == 200

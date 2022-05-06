1837f4a7c38a419c2c6e1b2afa58e5c88e914003
Brian Xian <brianxian2607@gmail.com>
2022-05-05 20:27:34 -0700

    added test for rendering the base of the website

--- test_app.py
+++ test_app.py
@@ -2,6 +2,24 @@ import pytest
 import flask
 import requests
 
+#make sure app.py is running before running the test
+
+@pytest.fixture
+def app():
+    from app import app
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+def test_request_index(client):
+    response = client.get("/")
+    assert b"<h2>Click on the plus button below to create a new grocery list!!</h2>"
 
 def test_api_grocery_list():
     response = requests.get("http://localhost:5000/api/grocery_list")

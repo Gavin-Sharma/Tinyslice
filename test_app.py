87b25656014ef852c4cb843066991b6988ee2163
Brian Xian <brianxian2607@gmail.com>
2022-05-05 20:56:37 -0700

    fixed typo in test_request_contact and changed test_api_grocery_list to follow the preceding tests

--- test_app.py
+++ test_app.py
@@ -21,12 +21,10 @@ def test_request_index(client):
     response = client.get("/")
     assert b"<h2>Click on the plus button below to create a new grocery list!!</h2>" in response.data
 
-def test_request_index(client):
+def test_request_contact(client):
     response = client.get("/contact")
     assert b"<h1 class=\"contact-hone\">Contact us</h1>" in response.data
 
-def test_api_grocery_list():
-    response = requests.get("http://localhost:5000/api/grocery_list")
-    data = response.json()
-    assert response.status_code == 200
-    assert data[0]["name"] == "Apple"
+def test_api_grocery_list(client):
+    response = client.get("/api/grocery_list")
+    assert response.json[0]["name"] == "Apple"

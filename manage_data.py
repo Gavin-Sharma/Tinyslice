32676fcc6044c27b4624ed06380aae771b9c68fc
Brian Xian <brianxian2607@gmail.com>
2022-05-10 21:08:33 -0700

    created function to remove grocery item from grocery list

--- manage_data.py
+++ manage_data.py
@@ -55,6 +55,28 @@ def delete_list(name):
         #returns the data
         return json_data
 
+def delete_item(grocery_list, item):
+    """Deletes grocery items from grocery list.
+    The grocery_list arugment is from which grocery list to delete the item from.
+    The item argument is what item to delete from the gorcery list."""
+    # open data.json
+    with open("static_files/data.json") as fp:
+        # load the json as a python object
+        json_data = json.load(fp)
+        # find the index of the grocery list that matches the name of the grocery list
+        index = next((index for (index, d) in enumerate(json_data) if d["list_name"] == grocery_list), None)
+        # loop through the grocery items in the grocery list
+        # the grocery list is accessed using the index found above
+        for groceries in json_data[index]["grocery"]:
+            # if the grocery item matches the item argument, remove the grocery item from the groceries list
+            if groceries[0] == item:
+                json_data[index]["grocery"].remove(groceries)
+                
+        # this will save the data
+        # json.dump(json_data, fp)
+
+        # return the data
+        return json_data
 
 def main():
     pass

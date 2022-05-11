08acecc9b9e0273a55f2d4f35a71e39c7f1e10c3
Brian Xian <brianxian2607@gmail.com>
2022-05-10 19:47:01 -0700

    created test for deleting a grocery list

--- test_manage_data.py
+++ test_manage_data.py
@@ -1,5 +1,6 @@
 from unittest.mock import mock_open, patch
 import manage_data
+import json
 
 #fake json, his is a json string
 FAKE_DATA = """[
@@ -29,3 +30,9 @@ def test_read(mock_file):
     assert json_data[0]["grocery"][0][1] == "5"
     assert json_data[0]["grocery"][1][0] == "pears"
     assert json_data[0]["grocery"][1][1] == "4"
+
+@patch("builtins.open", new_callable=mock_open, read_data = FAKE_DATA)
+def test_delete_list(mock_file):
+    json_data = manage_data.delete_list("list 1")
+    for grocery_list in json_data:
+        assert grocery_list["list_name"] != "list 1"

f1116ad92411cb1011cda79810ac47faa2f5b559
Brian Xian <brianxian2607@gmail.com>
2022-05-11 14:33:18 -0700

    updated test with comments

--- test_manage_data.py
+++ test_manage_data.py
@@ -47,15 +47,25 @@ def test_read(mock_file):
 
 @patch("builtins.open", new_callable=mock_open, read_data = FAKE_DATA)
 def test_delete_list(mock_file):
+    # deletes the grocery list with the name "list 1"
+    # the corrected data is returned
     json_data = manage_data.delete_list("list 1")
+    # verifies that there is no list called "list 1"
     for grocery_list in json_data:
         assert grocery_list["list_name"] != "list 1"
+    # verifies if "list 2" is not deleted
     assert json_data[0]["list_name"] == "list 2"
 
 @patch("builtins.open", new_callable=mock_open, read_data = FAKE_DATA)
 def test_delete_item(mock_file):
+    # deletes the "apples" item from "list 1"
+    # the corrected data is returned
     json_data = manage_data.delete_item("list 1", "apples")
+    # obtains the index for the list called "list 1"
+    # in our case index is 0
     index = next((index for (index, d) in enumerate(json_data) if d["list_name"] == "list 1"), None)
+    #access the grocery list at index 0
     grocery_list = json_data[index]
+    # verifies if the "apples" item is not included in the list
     for groceries in grocery_list:
         assert groceries[0] != "apples"

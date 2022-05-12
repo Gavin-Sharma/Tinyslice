07747a5ca18876c62c9c39106ed69a280e62efa2
Chris <chanwoooolee@gmail.com>
2022-05-11 20:09:40 -0700

    changed 'int' to 'float' in manage_data.py &  added unit test methods for test_manage_data.py

--- test_manage_data.py
+++ test_manage_data.py
@@ -1,8 +1,10 @@
 from unittest.mock import mock_open, patch
 import manage_data
 import json
+import pytest
 
-#fake json, his is a json string
+
+# fake json, his is a json string
 FAKE_DATA = """[
     {
         "list_name": "list 1",
@@ -34,7 +36,9 @@ FAKE_DATA = """[
 ]"""
 
 # opens the fake json as if it were a real file
-@patch("builtins.open", new_callable=mock_open, read_data = FAKE_DATA)
+
+
+@patch("builtins.open", new_callable=mock_open, read_data=FAKE_DATA)
 def test_read(mock_file):
     # call the function, it opens the fake json instead of the actual file
     # read function returns the json data as a python dictionary
@@ -45,7 +49,8 @@ def test_read(mock_file):
     assert json_data[0]["grocery"][1][0] == "pears"
     assert json_data[0]["grocery"][1][1] == "4"
 
-@patch("builtins.open", new_callable=mock_open, read_data = FAKE_DATA)
+
+@patch("builtins.open", new_callable=mock_open, read_data=FAKE_DATA)
 def test_delete_list(mock_file):
     # deletes the grocery list with the name "list 1"
     # the corrected data is returned
@@ -56,16 +61,42 @@ def test_delete_list(mock_file):
     # verifies if "list 2" is not deleted
     assert json_data[0]["list_name"] == "list 2"
 
-@patch("builtins.open", new_callable=mock_open, read_data = FAKE_DATA)
+
+@patch("builtins.open", new_callable=mock_open, read_data=FAKE_DATA)
 def test_delete_item(mock_file):
     # deletes the "apples" item from "list 1"
     # the corrected data is returned
     json_data = manage_data.delete_item("list 1", "apples")
     # obtains the index for the list called "list 1"
     # in our case index is 0
-    index = next((index for (index, d) in enumerate(json_data) if d["list_name"] == "list 1"), None)
-    #access the grocery list at index 0
+    index = next((index for (index, d) in enumerate(
+        json_data) if d["list_name"] == "list 1"), None)
+    # access the grocery list at index 0
     grocery_list = json_data[index]
     # verifies if the "apples" item is not included in the list
     for groceries in grocery_list:
         assert groceries[0] != "apples"
+
+
+@patch("builtins.open", new_callable=mock_open, read_data=FAKE_DATA)
+def test_save(mock_file):
+    manage_data_list = []
+    f = open('static_files/data.json', 'w')
+    f.write(json.dumps(manage_data_list))
+    mock_file.assert_called_once()
+
+
+def test_do_dict():
+    fake_list = manage_data.to_dict("List 1", "Apple", 0.99)
+    assert fake_list == {"list_name": "List 1",
+                         "total_cost": 0,
+                         "grocery": [["Apple", 0.99]]
+                         }
+
+
+def test_get_total_cost():
+    pass
+
+
+def test_save_total_cost():
+    pass
